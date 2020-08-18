ec16e62a144a7b39165ef08a0d5787c616b01dee
Omar Parkes <7733575+omrprks@users.noreply.github.com>
2020-08-18 14:30:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# template-node-library
+> Template containing commonly used config and package dependencies for node libraries
+
+## License
+[MIT](./LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export const sum = (a: number, b: number): number => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log('boop');
+  }
+
+  return a + b;
+};
+
+export default sum;
